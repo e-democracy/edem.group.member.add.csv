a801825eaec91bef529d2cf1fd144d73c950741b
Bill Bushey <wbushey@gmail.com>
2013-09-26 08:47:25 -0500

    Initial Commit based on r190 of gs.skin.ogn.edem

---
+++ README.txt
@@ -0,0 +1,33 @@
+==============================
+``edem.group.member.add.csv``
+==============================
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+E-Democracy customization of the add members by CSV code in 'GroupServer'_
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Author: `Bill Bushey`_
+:Contact: Bill Bushey <bill.bushey@e-democracy.org>
+:Date: 2013-09-26
+:Organization: `E-Democracy.org`_
+:Copyright: This document is licensed under a
+  `Creative Commons Attribution-Share Alike 3.0`_
+  by `E-Democracy.org`_.
+
+Introduction
+===========
+
+This egg customizes the browser view and processing associated with adding
+members to a group via CSV. 
+
+Resources
+=========
+
+- Code repository: https://github.com/e-democracy/edem.group.member.add.csv
+- Questions and comments to http://groupserver.org/groups/development
+- Report bugs at https://redmine.iopen.net/projects/groupserver
+
+.. _GroupServer: http://groupserver.org/
+.. _E-Democracy.org: http://e-democracy.org/
+.. _Bill Bushey: http://groupserver.org/p/wbushey
+.. _Creative Commons Attribution-Share Alike 3.0 License:
+   http://creativecommons.org/licenses/by-sa/3.0/
---
+++ edem/group/member/add/csv/create_users_from_csv.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+from gs.group.member.invite.csv.create_users_from_csv import CreateUsersInviteForm, CreateUsersAddForm, CreateUsersAddSiteForm
+from gs.skin.ogn.edem.utils import fn_to_nickname
+import logging
+log = logging.getLogger('EDemCreateUsersForm')
+
+class EDemCreateUsersForm(CreateUsersInviteForm):
+    def create_user(self, fields):
+        userInfo = CreateUsersInviteForm.create_user(self, fields)
+        if userInfo.nickname == userInfo.id:
+            m = 'Adding nickname to %s (%s)' % (userInfo.name, userInfo.id)
+            log.info(m)
+            nickname = fn_to_nickname(self.context, userInfo.name)
+            userInfo.user.add_nickname(nickname)
+        return userInfo
+
+class EDemUsersAddForm(EDemCreateUsersForm):
+    invite = False
+
+class EDemUsersAddSiteForm(CreateUsersAddSiteForm):
+    invite = False
+
+    def create_user(self, fields):
+        userInfo = CreateUsersInviteForm.create_user(self, fields)
+        if userInfo.nickname == userInfo.id:
+            m = 'Adding nickname to %s (%s)' % (userInfo.name, userInfo.id)
+            log.info(m)
+            nickname = fn_to_nickname(self.context, userInfo.name)
+            userInfo.user.add_nickname(nickname)
+        return userInfo
