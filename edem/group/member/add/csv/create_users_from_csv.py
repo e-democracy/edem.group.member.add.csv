d6e054b7612084289b9771c7728d3d7f1978dac6
Bill Bushey <wbushey@gmail.com>
2013-09-26 08:52:52 -0500

    PEP-8ify and add super-calls to the CSV-forms

--- edem/group/member/add/csv/create_users_from_csv.py
+++ edem/group/member/add/csv/create_users_from_csv.py
@@ -1,11 +1,17 @@
-# coding=utf-8
-from gs.group.member.invite.csv.create_users_from_csv import CreateUsersInviteForm, CreateUsersAddForm
+# -*- coding: utf-8 *-*
+from gs.group.member.invite.csv.create_users_from_csv import \
+    CreateUsersInviteForm
 from gs.group.member.invite.csv.site import CreateUsersAddSiteForm
 from utils import fn_to_nickname
 import logging
 log = logging.getLogger('EDemCreateUsersForm')
 
+
 class EDemCreateUsersForm(CreateUsersInviteForm):
+
+    def __init__(self, context, request):
+        super(EDemCreateUsersForm, self).__init__(context, request)
+
     def create_user(self, fields):
         userInfo = CreateUsersInviteForm.create_user(self, fields)
         if userInfo.nickname == userInfo.id:
@@ -15,11 +21,19 @@ class EDemCreateUsersForm(CreateUsersInviteForm):
             userInfo.user.add_nickname(nickname)
         return userInfo
 
+
 class EDemUsersAddForm(EDemCreateUsersForm):
     invite = False
 
+    def __init__(self, context, request):
+        super(EDemUsersAddForm, self).__init__(context, request)
+
+
 class EDemUsersAddSiteForm(CreateUsersAddSiteForm):
     invite = False
+
+    def __init__(self, context, request):
+        super(EDemUsersAddSiteForm, self).__init__(context, request)
 
     def create_user(self, fields):
         userInfo = CreateUsersInviteForm.create_user(self, fields)
